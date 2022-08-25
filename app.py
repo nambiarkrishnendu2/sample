e7cd240ffc880f1a7a1f1ea299c4de8fd1f8860f
nambiarkrishnendu2 <92816438+nambiarkrishnendu2@users.noreply.github.com>
2022-08-25 11:41:53 +0530

    Update app.py
    a sample

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import streamlit as st
-st.header("Hii")
+st.set_page_config(page_title="My webpage",page_icon=":tada:",layout="wide")
+st.subheader("Hi, I am Sven :wave:")
+st.title("A Data Analayst From Germany")
+st.write("I am passionate about finding way to use python and VBA to be more efficient")
+st.write("[Learn more >](https://pythonandvba.com)")
