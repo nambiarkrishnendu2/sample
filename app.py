f61a9e5d2add805cc7006a531284498c6e77ea3f
nambiarkrishnendu2 <92816438+nambiarkrishnendu2@users.noreply.github.com>
2022-08-25 16:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,56 +63,30 @@ with st.container():
             Others say as many as 17 percent of people show signs of reading challenges.
             """
         )
-# ---- PROJECTS ----
-with st.container():
-    st.write("---")
-    st.header("My Projects")
-    st.write("##")
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        st.image(img_lottie_animation)
-    with text_column:
-        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
-        st.write(
-            """
-            Learn how to use Lottie Files in Streamlit!
-            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
-            In this tutorial, I'll show you exactly how to do it
-            """
+        with left_column:
+            st.subheader("Dyslexia signs and symptoms")
+            st.write("##")
+            st.write(
+                """
+                Dyslexia impacts people in different ways. 
+                So, symptoms might not look the same from one person to another.
+                A key sign of dyslexia is trouble decoding words.
+                This is the ability to match letters to sounds.
+                Kids can also struggle with a more basic skill called phonemic awareness.
+                This is the ability to recognize the sounds in words.
+                Trouble with phonemic awareness can show up as early as preschool.
+                In some people, dyslexia isn’t picked up until later on, when they have trouble with more complex skills. 
+                These can include grammar, reading comprehension, reading fluency, sentence structure, and more in-depth writing.
+                Some of the signs of dyslexia have to do with emotions and behavior.
+                People with dyslexia might avoid reading, both out loud and to themselves. 
+                They may even get anxious or frustrated when reading.
+                This can happen even after they’ve mastered the basics of reading.
+                
+                """
         )
-        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
-with st.container():
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        st.image(img_contact_form)
-    with text_column:
-        st.subheader("How To Add A Contact Form To Your Streamlit App")
-        st.write(
-            """
-            Want to add a contact form to your Streamlit website?
-            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
-            """
-        )
-        st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
 
-# ---- CONTACT ----
-with st.container():
-    st.write("---")
-    st.header("Get In Touch With Me!")
-    st.write("##")
+        
+        
+       
 
-    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    contact_form = """
-    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Your name" required>
-        <input type="email" name="email" placeholder="Your email" required>
-        <textarea name="message" placeholder="Your message here" required></textarea>
-        <button type="submit">Send</button>
-    </form>
-    """
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.markdown(contact_form, unsafe_allow_html=True)
-    with right_column:
-        st.empty()
+
