feae007373736cbb02b4966063e0d0e6236d714d
nambiarkrishnendu2 <92816438+nambiarkrishnendu2@users.noreply.github.com>
2022-08-25 12:11:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with st.container():
         import json
         import requests
         from streamlit_lottie import st_lottie
-        def load_lottie(url: str:):
+        def load_lottie(url: str):
             r=requests.get(url)
             if r.status_code != 200:
                 return None
